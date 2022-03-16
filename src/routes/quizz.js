9aab3ab7fe6b92bccd20119c211ad63ded895aed
Mickaseg <mickael_seguin@hotmail.fr>
2022-03-17 00:09:23 +0100

    added routes for themes

--- src/routes/quizz.js
+++ src/routes/quizz.js
@@ -2,9 +2,28 @@ const express = require("express");
 const mysql = require("../config/db.js");
 const Router = express.Router();
 
+Router.get("/questions/theme1", (req, res) => {
+  const sql =
+    "SELECT q.id, q.question_text FROM questions AS q INNER JOIN theme AS t ON t.id=q.theme_id WHERE t.id= 1;";
+  mysql.query(sql, (err, result) => {
+    if (err) throw err;
+    else return res.status(200).json(result);
+  });
+});
+
+Router.get("/questions/theme2", (req, res) => {
+  const sql =
+    "SELECT q.id, q.question_text FROM questions AS q INNER JOIN theme AS t ON t.id=q.theme_id WHERE t.id= 2;";
+  mysql.query(sql, (err, result) => {
+    if (err) throw err;
+    else return res.status(200).json(result);
+  });
+});
+
 Router.get("/questions/:id", (req, res) => {
   const id = req.params.id;
-  const sql = "SELECT q.id, q.question_text FROM questions AS q WHERE id= ?";
+  const sql =
+    "SELECT q.id, q.question_text FROM questions AS q INNER JOIN theme AS t ON t.id=q.theme_id WHERE q.id= ?";
   mysql.query(sql, id, (err, result) => {
     // console.log(result);
     if (err) throw err;
