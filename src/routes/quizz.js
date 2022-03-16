6ba1274f39afe3c169de21ac66d7647438af152e
Mickaseg <mickael_seguin@hotmail.fr>
2022-03-16 18:50:15 +0100

    added route for questions

--- src/routes/quizz.js
+++ src/routes/quizz.js
@@ -2,4 +2,14 @@ const express = require("express");
 const mysql = require("../config/db.js");
 const Router = express.Router();
 
+Router.get("/:id", (req, res) => {
+  const id = req.params.id;
+  const sql =
+    "SELECT q.id, q.question_text, a.answer_text, a.isCorrect FROM questions AS q INNER JOIN answers AS a ON q.id=a.questions_id WHERE q.id = ?";
+  mysql.query(sql, id, (err, result) => {
+    // console.log(result);
+    if (err) throw err;
+    else return res.status(200).json(result);
+  });
+});
 module.exports = Router;
