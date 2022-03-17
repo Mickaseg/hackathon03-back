c5591d6f3cea51d2c7c4c6cd77575fac325165a0
GRATADE Jean-François <gratade.jeanfrancois@gmail.com>
2022-03-17 17:57:56 +0100

    articles routes update

--- src/routes/articles.js
+++ src/routes/articles.js
@@ -3,10 +3,10 @@ const mysql = require("../config/db.js");
 const Router = express.Router();
 
 
-Router.get("/articles/:id/questions", (req, res) => {
+Router.get("/:id/", (req, res) => {
     const id = req.params.id;
     const sql =
-    "SELECT q.id, q.question_text, q.theme_id, q.image FROM articles AS a INNER JOIN questions AS q ON a.id=q.articles_id WHERE a.id = ?";
+    "SELECT * FROM articles AS a INNER JOIN questions AS q ON a.id=q.articles_id WHERE q.id = ?";
     mysql.query(sql, id, (err, result) => {
       // console.log(result);
       if (err) throw err;
