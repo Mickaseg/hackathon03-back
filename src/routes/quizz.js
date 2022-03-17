ee2de6b05ca8b048b931dc88521355fa4df2433b
Marsaleix Romain <msx.coach@gmail.com>
2022-03-17 12:50:24 +0100

    save before dev pull

--- src/routes/quizz.js
+++ src/routes/quizz.js
@@ -2,11 +2,12 @@ const express = require("express");
 const mysql = require("../config/db.js");
 const Router = express.Router();
 
+
 Router.get("/questions/:id", (req, res) => {
   const id = req.params.id;
   const sql = "SELECT q.id, q.question_text FROM questions AS q WHERE id= ?";
   mysql.query(sql, id, (err, result) => {
-    // console.log(result);
+    console.log(result);
     if (err) throw err;
     else return res.status(200).json(result);
   });
@@ -17,7 +18,7 @@ Router.get("/questions/:id/answers", (req, res) => {
   const sql =
     "SELECT a.id, a.answer_text, a.isCorrect FROM questions AS q INNER JOIN answers AS a ON q.id=a.questions_id WHERE q.id = ?";
   mysql.query(sql, id, (err, result) => {
-    // console.log(result);
+    console.log(result);
     if (err) throw err;
     else return res.status(200).json(result);
   });
